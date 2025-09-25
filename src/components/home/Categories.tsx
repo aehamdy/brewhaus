cf5a9a8c6563dbe45668cecb22be14bc978256f9
AhmedEssam <ae.devverse@gmail.com>
2025-09-25 18:43:01 +0300

    Chore: Add Category type and implement Section component

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -4,10 +4,18 @@ import coldDrinksCategory from "@/assets/cold-drinks-category.webp";
 import bakeryCategory from "@/assets/bakery-category.webp";
 import { ROUTES } from "@/lib/routes";
 import Link from "next/link";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Heading from "@/components/common/Heading";
+import Section from "@/components/common/Section";
 
-const categories = [
+type Category = {
+  id: number;
+  title: string;
+  image: StaticImageData;
+  link: string;
+};
+
+const categories: Category[] = [
   {
     id: 1,
     title: "Coffee",
@@ -30,42 +38,44 @@ const categories = [
 
 function Categories() {
   return (
-    <section className="h-[94dvh] md:h-[75dvh] lg:min-h-dvh py-[104px] md:py-[160px] space-y-[48px] md:space-y-[80px] bg-surface-primary border-b border-subtle">
-      <div className="flex flex-col items-center gap-2xl md:gap-4xl">
-        <SectionTitle variant="primary" align="center" dark>
-          <div className="">Find and Get</div>
-          <div className="">What You Love</div>
-        </SectionTitle>
-      </div>
+    <Section background="primary" border>
+      <div className="flex flex-col gap-[48px] md:gap-[80px]">
+        <div className="flex flex-col items-center gap-2xl md:gap-4xl">
+          <SectionTitle variant="primary" align="center" dark>
+            <div className="">Find and Get</div>
+            <div className="">What You Love</div>
+          </SectionTitle>
+        </div>
 
-      <ul className="grid grid-cols-1 md:grid-cols-3 gap-lg md:gap-md px-xl">
-        {categories.map((category) => (
-          <li key={category.id}>
-            <Link
-              href={category.link}
-              className="group flex md:flex-col items-center gap-lg px-large lg:px-2xl"
-            >
-              <div className="w-[160px] md:w-[173px] lg:w-[288px] h-[160px] md:h-[173px] lg:h-[288px] bg-brand-primary rounded-full overflow-hidden">
-                <Image
-                  src={category.image}
-                  alt={category.title}
-                  width={200}
-                  height={200}
-                  className="w-full h-full group-hover:scale-105 object-cover transition-all duration-slowest"
-                />
-              </div>
-
-              <Heading
-                level={3}
-                className="font-calistoga text-2xl md:text-xlarge text-brand-primary group-hover:text-brand-secondary transition-all duration-normal"
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-lg md:gap-md px-xl">
+          {categories.map((category) => (
+            <li key={category.id}>
+              <Link
+                href={category.link}
+                className="group flex md:flex-col items-center gap-lg px-large lg:px-2xl"
               >
-                {category.title}
-              </Heading>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </section>
+                <div className="w-[160px] md:w-[173px] lg:w-[288px] h-[160px] md:h-[173px] lg:h-[288px] bg-brand-primary rounded-full overflow-hidden">
+                  <Image
+                    src={category.image}
+                    alt={category.title}
+                    width={200}
+                    height={200}
+                    className="w-full h-full group-hover:scale-105 object-cover transition-all duration-slowest"
+                  />
+                </div>
+
+                <Heading
+                  level={3}
+                  className="font-calistoga text-2xl md:text-xlarge text-brand-primary group-hover:text-brand-secondary transition-all duration-normal"
+                >
+                  {category.title}
+                </Heading>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Section>
   );
 }
 
