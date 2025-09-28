74d49aaf13cbda1b7f6709a4f73a8e19f5978e5d
AhmedEssam <ae.devverse@gmail.com>
2025-09-28 21:48:56 +0300

    Feat: Create home related components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import HeroSection from "@/components/home/HeroSection";
+import MarqueeText from "@/components/common/MarqueeText";
+import Categories from "@/home/Categories";
+import HeroSection from "@/home/HeroSection";
+import Products from "@/home/Products";
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
+      <MarqueeText />
+      <Categories />
+      <Products />
     </main>
   );
 }
