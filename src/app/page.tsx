80f029f331a2edc6bcf25beb698d5332aad77c2e
AhmedEssam <ae.devverse@gmail.com>
2025-09-30 01:03:41 +0300

    Feat: Add Nearby and WhatsNew sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import MarqueeText from "@/components/common/MarqueeText";
 import Categories from "@/home/Categories";
 import HeroSection from "@/home/HeroSection";
+import Nearby from "@/home/Nearby";
 import OrderOnline from "@/home/OrderOnline";
 import Products from "@/home/Products";
 import Testimonials from "@/home/Testimonials";
 import Vibes from "@/home/Vibes";
+import WhatsNew from "@/home/WhatsNew";
 
 export default function Home() {
   return (
@@ -15,6 +17,8 @@ export default function Home() {
       <Products />
       <Vibes />
       <Testimonials />
+      <Nearby />
+      <WhatsNew />
       <OrderOnline />
     </main>
   );
