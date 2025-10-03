f1edb7cba5c0102479af93c21eedb16452fe6adb
AhmedEssam <ae.devverse@gmail.com>
2025-10-03 12:03:43 +0300

    Chore: Invoke 'WhatsNewCard' component instead of hardcode it

--- src/home/WhatsNew.tsx
+++ src/home/WhatsNew.tsx
@@ -1,10 +1,9 @@
 import Button from "@/components/common/Button";
 import Section from "@/components/common/Section";
 import SectionTitle from "@/components/common/SectionTitle";
+import WhatsNewCard from "@/components/common/WhatsNewCard";
 import whatsNewList from "@/data/whatsNew";
 import { ROUTES } from "@/lib/routes";
-import Image from "next/image";
-import Link from "next/link";
 
 function WhatsNew() {
   return (
@@ -27,28 +26,9 @@ function WhatsNew() {
         </div>
 
         <ul className="grid grid-cols-1 lg:grid-cols-3 gap-xl lg:gap-md">
-          {whatsNewList.map((item) => (
+          {whatsNewList.slice(0, 3).map((item) => (
             <li key={item.id}>
-              <Link
-                href={`${ROUTES.news}/${item.id}`}
-                className="flex flex-col gap-base"
-              >
-                <div className="h-[382px] lg:h-[280px] rounded-lg overflow-hidden">
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2xs px-2xs">
-                  <p className="text-small text-brand-secondary">{item.date}</p>
-
-                  <p className="w-1/2 lg:w-9/10 font-calistoga text-large text-brand-primary leading-6">
-                    {item.title}
-                  </p>
-                </div>
-              </Link>
+              <WhatsNewCard item={item} />
             </li>
           ))}
         </ul>
