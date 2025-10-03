0259fdfda8641a91772c653906b48f0349e15a2c
AhmedEssam <ae.devverse@gmail.com>
2025-10-03 12:36:15 +0300

    Feat: Create 'locations' page

---
+++ src/app/locations/page.tsx
@@ -0,0 +1,25 @@
+import PageHero from "@/components/common/PageHero";
+import SectionTitle from "@/components/common/SectionTitle";
+import Nearby from "@/home/Nearby";
+
+function page() {
+  return (
+    <section>
+      <PageHero className="text-accent-highlight" background="dark">
+        <div className="flex flex-col items-center gap-sm">
+          <SectionTitle variant="hero" dark={false}>
+            Locations
+          </SectionTitle>
+
+          <p className="text-medium tracking-wide">
+            Find your nearest brewhaus location.
+          </p>
+        </div>
+      </PageHero>
+
+      <Nearby title={false} />
+    </section>
+  );
+}
+
+export default page;
