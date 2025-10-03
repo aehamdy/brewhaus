d6c84165b3dc82855255e7cadf4aa0bd11f3e652
AhmedEssam <ae.devverse@gmail.com>
2025-10-03 12:05:08 +0300

    Feat: Create WhatsNewCard reusable component

---
+++ src/components/common/WhatsNewCard.tsx
@@ -0,0 +1,41 @@
+import { ROUTES } from "@/lib/routes";
+import Image from "next/image";
+import Link from "next/link";
+import { WhatsNew } from "@/data/whatsNew";
+import Heading from "@/components/common/Heading";
+
+type Props = {
+  item: WhatsNew;
+};
+
+function WhatsNewCard({ item }: Props) {
+  return (
+    <article className="group">
+      <Link
+        href={`${ROUTES.news}/${item.id}`}
+        className="flex flex-col gap-base"
+      >
+        <div className="h-[382px] md:h-[332px] lg:h-[280px] rounded-lg overflow-hidden">
+          <Image
+            src={item.image}
+            alt={item.title}
+            className="w-full h-full object-cover group-hover:scale-105 transition-all duration-slow"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2xs px-2xs">
+          <p className="text-small text-brand-secondary">{item.date}</p>
+
+          <Heading
+            level={3}
+            className="lg:w-9/10 font-calistoga text-large text-brand-primary group-hover:text-brand-secondary leading-6"
+          >
+            {item.title}
+          </Heading>
+        </div>
+      </Link>
+    </article>
+  );
+}
+
+export default WhatsNewCard;
