4d8c248619047827b15845caf7147c41632c0c16
AhmedEssam <ae.devverse@gmail.com>
2025-10-06 02:04:20 +0300

    Feat: Create NewsArticlePage component

---
+++ src/app/news/[articleSlug]/page.tsx
@@ -0,0 +1,63 @@
+import NotFound from "@/app/not-found";
+import Button from "@/components/common/Button";
+import Heading from "@/components/common/Heading";
+import PageHero from "@/components/common/PageHero";
+import WhatsNewCard from "@/components/common/WhatsNewCard";
+import NewsArticleContent from "@/components/NewsArticleContent";
+import whatsNewList from "@/data/whatsNew";
+import { ROUTES } from "@/lib/routes";
+
+interface NewsArticlePageProps {
+  params: { articleSlug: string };
+}
+
+function NewsArticlePage({ params }: NewsArticlePageProps) {
+  const article = whatsNewList.find(
+    (article) => article.slug === params.articleSlug
+  );
+
+  if (!article) {
+    return NotFound();
+  }
+
+  return (
+    <main className="bg-surface-primary/85">
+      <PageHero className="text-accent-highlight" background="dark">
+        <div className=""></div>
+      </PageHero>
+
+      <div className="space-y-20">
+        <div className="lg:max-w-[720px] -mt-40 mx-md lg:mx-auto">
+          <NewsArticleContent article={article} />
+        </div>
+
+        <section className="lg:h-dvh py-[104px] lg:py-5xl bg-surface-secondary border-t border-t-subtle">
+          <div className="flex flex-col gap-2xl px-md md:px-xl lg:px-[100px]">
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-md">
+              <Heading
+                level={2}
+                className="font-calistoga text-2xl md:text-xlarge text-brand-primary"
+              >
+                More to Explore
+              </Heading>
+
+              <Button href={ROUTES.news} variant="primary" fullWidthOnMobile>
+                Explore News
+              </Button>
+            </div>
+
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-md">
+              {whatsNewList.slice(0, 3).map((article) => (
+                <li key={article.id}>
+                  <WhatsNewCard item={article} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default NewsArticlePage;
